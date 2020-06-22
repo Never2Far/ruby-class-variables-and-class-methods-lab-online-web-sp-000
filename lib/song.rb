1a64cd1d86575998a4fd37d701e5c9a5e385eb68
Seth Near <seth.j.near@gmail.com>
2020-06-22 16:14:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
 
 @@count = 0
 @@genres = []
+@@artists = []
 
 def initialize(name, artist, genre)
   @name = name
@@ -9,6 +10,7 @@ def initialize(name, artist, genre)
   @genre = genre
   @@count += 1
   @@genres << genre
+  @@artists << artist
 end
 
 attr_reader :name, :artist, :genre
@@ -19,6 +21,20 @@ end
 
 def self.genres
  return @@genres.uniq
+end
+
+def self.artists
+ return @@artists.uniq
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each { |genre|
+    genre_count[genre] ||= genre_count[genre] = 0
+    genre_count[genre] += 1
+
+  }
+
 end
 
 end
